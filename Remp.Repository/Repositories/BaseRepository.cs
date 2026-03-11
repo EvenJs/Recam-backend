a369567f951450350c328c99fa24fc5d47eeeff9
Johnson Wang <yuhang0229@gmail.com>
2026-03-11 15:32:47 +1100

    feat(repository): add IBaseRepository interface and BaseRepository generic implementation

---
+++ Remp.Repository/Repositories/BaseRepository.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Remp.DataAccess.Data;
+using Remp.Repository.Interfaces;
+
+namespace Remp.Repository.Repositories;
+
+public class BaseRepository<T> : IBaseRepository<T> where T : class
+{
+  protected readonly AppDbContext _context;
+  protected readonly DbSet<T> _dbSet;
+
+  public BaseRepository(AppDbContext context)
+  {
+    _context = context;
+    _dbSet = context.Set<T>();
+  }
+
+  public async Task<T?> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
+
+  public async Task<IEnumerable<T>> GetAllAsync() => await _dbSet.ToListAsync();
+
+  public async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
+
+  public async Task UpdateAsync(T entity) => _dbSet.Update(entity);
+
+  public async Task DeleteAsync(T entity) => _dbSet.Remove(entity);
+}
